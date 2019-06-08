6ea219a7bb3e2b367cede2e94d3c51ddaa7bb5cc
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-06-07 21:49:10 -0300

    Adapted to the new object model

--- public/addComment.php
+++ public/addComment.php
@@ -3,8 +3,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $cfg = require_once __DIR__ . '/../config/config.php';
 
-$trans = new Translator(require_once __DIR__ . '/../config/messages.php');
-$commentsRepo = new CommentsRepository( $cfg['comments_file'] );
+$trans = new MessageBag(require_once __DIR__ . '/../config/messages.php');
+$commentsSerializer = new CommentsJsonSerializer();
+$commentsRepo = new CommentsRepository( $cfg['comments_file'], $commentsSerializer );
 $comments = $commentsRepo->read();
 
 $comments[]  = new Comment(
--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 require_once __DIR__.'/../vendor/autoload.php';
 $cfg = require_once __DIR__.'/../config/config.php';
 
-$trans = new Translator( require_once __DIR__.'/../config/messages.php' );
-$commentsRepo = new CommentsRepository( $cfg['comments_file'] );
+$trans = new MessageBag( require_once __DIR__.'/../config/messages.php' );
+$commentsSerializer = new CommentsJsonSerializer();
+$commentsRepo = new CommentsRepository( $cfg['comments_file'], $commentsSerializer );
 $comments = $commentsRepo->read();
 ?>
 <html>
@@ -13,9 +14,9 @@ $comments = $commentsRepo->read();
     <table>
         <thead>
             <tr>
-                <th><?php echo $trans->translate('date_label'); ?></th>
-                <th><?php echo $trans->translate('author_label'); ?></th>
-                <th><?php echo $trans->translate('comment_label'); ?></th>
+                <th><?php echo $trans->getMessage('date_label'); ?></th>
+                <th><?php echo $trans->getMessage('author_label'); ?></th>
+                <th><?php echo $trans->getMessage('comment_label'); ?></th>
             </tr>
         </thead>
         <tbody>
@@ -34,11 +35,11 @@ $comments = $commentsRepo->read();
 </div>
 <div id="newComment">
     <form action="addComment.php" method="post">
-        <label for="author"><?php echo $trans->translate('author_label'); ?></label>
-        <input type="text" id="author" name="author" placeholder="<?php echo $trans->translate('input_author'); ?>"/>
-        <label for="comment"><?php echo $trans->translate('comment_label'); ?></label>
-        <input type="text" id="comment" name="comment" placeholder="<?php echo $trans->translate('input_comment'); ?>"/>
-        <input type="submit" value="<?php echo $trans->translate('submit'); ?>"/>
+        <label for="author"><?php echo $trans->getMessage('author_label'); ?></label>
+        <input type="text" id="author" name="author" placeholder="<?php echo $trans->getMessage('input_author'); ?>"/>
+        <label for="comment"><?php echo $trans->getMessage('comment_label'); ?></label>
+        <input type="text" id="comment" name="comment" placeholder="<?php echo $trans->getMessage('input_comment'); ?>"/>
+        <input type="submit" value="<?php echo $trans->getMessage('submit'); ?>"/>
     </form>
 </div>
 </body>
